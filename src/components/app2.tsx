f8a32d92c4294e8d6f7e57f51068abe218d0b178
shenxiaoqing <shenxiaoqing@sune365.com.cn>
2019-12-20 15:27:15 +0800

    update example

---
+++ src/components/app2.tsx
@@ -0,0 +1,7 @@
+import React, { useEffect, useState, useCallback, useRef } from 'react'
+
+function App (props: any){
+    return <div>Hello app2</div>
+}
+
+export default App
--- src/router.js
+++ src/router.js
@@ -1,7 +1,19 @@
 import App from './components/app'
+import App2 from './components/app2'
 import { prefix } from './const'
 
 export default [
+    {
+        path: `${prefix}/app2`,
+        component: App2,
+        api: ['mindmap', 'login'],
+        onEnter: e=>{
+            console.log('Enter app2')
+        },
+        onChange: e => {
+            // console.log('  --------------onChange', e)
+        }
+    },
     {
         path: `${prefix}/`,
         component: App,
